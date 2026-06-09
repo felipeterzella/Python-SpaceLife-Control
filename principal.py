513ea83be5374aab27e36523625a9aab5e93a9b2
felipeterzella <felipebragaterzella0@gmail.com>
2026-06-09 08:26:37 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,13 +1,14 @@
-from funcoes import relatorio, variaveis, calcular, cadastrar_recursos
+from funcoes import relatorio, variaveis, calcular, cadastrar_recursos, adicionar
 while True:
-    print("=============SpaceLife-Control=============")
+    print("======SISTEMA DE CONTROLE DE RECURSOS======")
     print("1 - Adicionar recursos")
     print("2 - Calcular autonomia")
     print("3 - Aplicar evento")
     print("4 - Ver relatorio")
-    print("5 - Sair")
+    print("5 - Adicionar mais recursos a quantidade original")
+    print("6 - Sair")
     print("===========================================")
-    opcao = input("ESCOLHA UMA OPÇÃO (1,2,3,4,5)")
+    opcao = input("ESCOLHA UMA OPÇÃO (1,2,3,4,5,6)")
     match opcao:
         case "1":
             cadastrar_recursos()
@@ -18,4 +19,6 @@ while True:
         case "4":
             relatorio()
         case "5":
+            adicionar()
+        case "6":
             break
